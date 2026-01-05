be31629bf27f59bef874405f35271b61b730b729
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-05 15:02:49 +0530

    Create blocks.js

---
+++ solana/blocks.js
@@ -0,0 +1,16 @@
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/solana/blocks/390740000_390740049.parquet
+/* More files in S3 under:
+bitquery-blockchain-dataset/solana/blocks/
+*/
+
+  {
+    "Block_Date":1767312000000,
+    "Block_Hash":"4PcvZtBdidGwGkEdnwCXPjDyKPSyD5put6mjGPYBPVoM",
+    "Block_Height":368880188,
+    "Block_ParentHash":"GQ2qbrmS8D5Q5AdKsz5p3VUif46iueb57qm4e8QdMGuy",
+    "Block_ParentSlot":390739999,
+    "Block_RewardsCount":1,
+    "Block_Slot":390740000,
+    "Block_Time":1767323643,
+    "Block_TxCount":1104
+  }
