4d30681616d9c68543bf918ecf7e36c39ead7b63
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-20 15:13:41 +0530

    Create outputs.js

---
+++ bitcoin/outputs.js
@@ -0,0 +1,30 @@
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/bitcoin/outputs/932100_932149.parquet
+// more files in
+//   bitquery-blockchain-dataset/
+// ├── bitcoin/
+  {
+    "tx_date":1768262400000,
+    "tx_time":1768295973,
+    "blockchain_id":0,
+    "block":932100,
+    "block_timestamp":1768295973,
+    "output_address":"37jKPSmbEGwgfacCr2nayn1wTaqMAbA94Z",
+    "output_address_annotation":null,
+    "date":"2026-01-13",
+    "direction":"mining",
+    "output_index":0,
+    "output_script":"OP_HASH160 42402a28dd61f2718a4b27ae72a4791d5bbdade7 OP_EQUAL",
+    "output_script_type_annotation":null,
+    "script_template":28,
+    "script_type":"scripthash",
+    "output_script_type_pattern":"HASH160 {1} EQUAL",
+    "output_script_type_short":"HASH160 {1} EQUAL",
+    "output_script_type_simplePattern":"HASH160 {1} EQUAL",
+    "output_script_type_type":"scripthash",
+    "req_sigs":0,
+    "tx_hash":"93ccd839d16463487296cc4f3c1376a9ad806ab4283d36cf8ea5c2d87b488f70",
+    "transaction_index":0,
+    "value_decimal":0.00000546,
+    "value_in_usd":0.5028633553,
+    "value":0.00000546
+  }
