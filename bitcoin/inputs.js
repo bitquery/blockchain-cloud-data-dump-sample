7c9b455489ed14c37dbfa83616ac216f36e0d414
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-20 15:12:35 +0530

    Create inputs.js

---
+++ bitcoin/inputs.js
@@ -0,0 +1,28 @@
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/bitcoin/inputs/932100_932149.parquet
+// more files in
+//   bitquery-blockchain-dataset/
+// ├── bitcoin/
+
+  {
+    "tx_date":1768262400000,
+    "tx_time":1768295973,
+    "blockchain_id":0,
+    "block":932100,
+    "tx_hash":"cb0ade8ac834ce9a2743a9cf79c0f83d28302b14abc0206e6a3037dcbd559665",
+    "transaction_index":1009,
+    "output_tx_hash":"9c4cb614bced7a7254b2eb6ba9db538d8f78fa98d5beede527c715022893c63e",
+    "output_transaction_index":1,
+    "input_index":0,
+    "input_address":"bc1pz0gk3z52umw940pau6dg4zszh0x0cksrsl69265w9qqarm2s96wspxl5f4",
+    "input_address_annotation":null,
+    "input_script":"e967e7eeebbca62361615b95f197c758ae98534654cfe4867c570822699b8f214c4788217a324041e962832419b6d03048c40f806d6ad69280e765d40e2e1249",
+    "script_template":1,
+    "input_script_type_annotation":"none",
+    "input_script_type_pattern":"",
+    "input_script_type_simplePattern":"",
+    "input_script_type_type":"txinwitness",
+    "value_decimal":0.00001,
+    "value_in_usd":0.9209951562,
+    "value":0.00001
+  }
+
