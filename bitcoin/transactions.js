e65dea0ad988d57ff9a0e5727f0c22e76059a91e
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-20 15:12:58 +0530

    Create transactions.js

---
+++ bitcoin/transactions.js
@@ -0,0 +1,32 @@
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/bitcoin/transactions/932100_932149.parquet
+// more files in
+//   bitquery-blockchain-dataset/
+// ├── bitcoin/
+  {
+    "tx_date":1768262400000,
+    "tx_time":1768295973,
+    "blockchain_id":0,
+    "block":932100,
+    "tx_hash":"93ccd839d16463487296cc4f3c1376a9ad806ab4283d36cf8ea5c2d87b488f70",
+    "tx_index":0,
+    "locktime":0,
+    "version":1,
+    "size":474,
+    "vsize":447,
+    "weight":1788,
+    "is_coinbase":1,
+    "input_count":1,
+    "output_count":7,
+    "input_value_decimal":0.0,
+    "output_value_decimal":3.13445868,
+    "fee_value_decimal":0.0,
+    "mined_value_decimal":3.13445868,
+    "input_value":0.0,
+    "output_value":3.13445868,
+    "fee_value":0.0,
+    "mined_value":3.13445868,
+    "input_value_in_usd":0.0,
+    "output_value_in_usd":288682.1261745769,
+    "fee_value_in_usd":0.0
+  }
+
