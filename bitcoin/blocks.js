50a9e17b95b67950985f509945489f540d1c7764
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-20 11:57:03 +0530

    Create blocks.js

---
+++ bitcoin/blocks.js
@@ -0,0 +1,19 @@
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/bitcoin/blocks/932100_932149.parquet
+// more files in
+//   bitquery-blockchain-dataset/
+// ├── bitcoin/
+
+  {
+    "tx_date":1768262400000,
+    "tx_time":1768295973,
+    "block":932100,
+    "median_time":1768295973,
+    "difficulty":146472570619930.8125,
+    "hash":"00000000000000000000a2169fd41c2589f17ac39ae0d31f85af4300bb091ee1",
+    "strippedsize":671152,
+    "size":1980553,
+    "weight":3994009,
+    "version":537395200,
+    "txcount":1321,
+    "chainwork":"000000000000000000000000000000000000000106933d78f29f5e96d3048755"
+  }
