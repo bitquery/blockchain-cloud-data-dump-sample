7e6e45f3465742b5fc0740ce9a77f7666532b0f1
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-20 16:48:49 +0530

    Create omni_transactions.js

---
+++ bitcoin/omni_transactions.js
@@ -0,0 +1,22 @@
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/bitcoin/omni_transactions/859350_859399.parquet
+// more files in
+//   bitquery-blockchain-dataset/
+// ├── bitcoin/
+
+  {
+    "tx_date":1725148800000,
+    "tx_time":1725188908,
+    "blockchain_id":0,
+    "block":859366,
+    "block_hash":"000000000000000000014e92ca90fc024de721ca7591d1ce3664bf435573306a",
+    "tx_hash":"9fc262f79523e02df9e8738779b9e91c8dc8add1e0c5b1067420b2700a53f48f",
+    "tx_index":1,
+    "type":"Create Property - Fixed",
+    "type_int":50,
+    "tx_sender":"1CLAudzPKzt7tnd7XNy6n6UJGtzGYf1jWB",
+    "valid":1,
+    "version":0,
+    "json":"%7B%22txid%22:%229fc262f79523e02df9e8738779b9e91c8dc8add1e0c5b1067420b2700a53f48f%22,%22fee%22:%220.00220324%22,%22sendingaddress%22:%221CLAudzPKzt7tnd7XNy6n6UJGtzGYf1jWB%22,%22ismine%22:false,%22version%22:0,%22type_int%22:50,%22type%22:%22Create Property - Fixed%22,%22propertyid%22:2147484212,%22divisible%22:true,%22ecosystem%22:%22test%22,%22propertytype%22:%22divisible%22,%22category%22:%22Other%22,%22subcategory%22:%22Other%22,%22propertyname%22:%22BTC%22,%22data%22:%22%22,%22url%22:%22%22,%22amount%22:%22728.00000000%22,%22valid%22:true,%22blockhash%22:%22000000000000000000014e92ca90fc024de721ca7591d1ce3664bf435573306a%22,%22blocktime%22:1725188908,%22positioninblock%22:1,%22block%22:859366,%22confirmations%22:2%7D",
+    "fee_value":0.00220324,
+    "fee_value_usd":127.9644804242
+  }
