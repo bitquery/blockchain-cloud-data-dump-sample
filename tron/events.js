2a3bc6cdf7212aaa77e114f6953e741784fdddb9
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-02-03 15:49:25 +0530

    Add TRON event data structure
    Added event data structure for TRON transactions, including arguments and transaction details.

---
+++ tron/events.js
@@ -0,0 +1,55 @@
+// This is a sample of the tron_events.parquet file
+// you can download more files from the following link: https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/tron/events/78861100_78861149.parquet
+//    │   ├── 78861100_78861149.parquet
+//    │   ├── 78861150_78861199.parquet
+//    │   ├── 78861200_78861249.parquet
+
+  {
+    "Arguments":"[[0, \"from\", [], \"address\", [\"address\", \"db5f3f3a9798f3d1f89b2f12b6acda2fb3c91f80\", \"\", 0, 0, \"false\"]], [1, \"to\", [], \"address\", [\"address\", \"29228e5d382b7a2c2a0dbc05e6d6507f4dbb3bbe\", \"\", 0, 0, \"false\"]], [2, \"value\", [], \"uint256\", [\"uint256\", \"\", \"30401259\", 0, 0, \"false\"]]]",
+    "Topics_Hash":"[\"ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef\", \"000000000000000000000000db5f3f3a9798f3d1f89b2f12b6acda2fb3c91f80\", \"00000000000000000000000029228e5d382b7a2c2a0dbc05e6d6507f4dbb3bbe\"]",
+    "Block_Number":78861100,
+    "Block_Time":1767246213,
+    "Block_Date":1767225600000,
+    "Call_Signature_Name":"transfer",
+    "Call_Signature_Signature":"transfer(address,uint256)",
+    "Call_Signature_SignatureHash":"a9059cbb",
+    "Call_Signature_Type":"false",
+    "Call_Signature_Abi":"{\"outputs\":[{\"type\":\"bool\"}],\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"stateMutability\":\"Nonpayable\",\"type\":\"Function\"}",
+    "Call_CallPath":[
+
+    ],
+    "Call_CallerIndex":-1,
+    "Call_InternalCalls":0,
+    "Call_SelfDestruct":"false",
+    "Call_Reverted":"false",
+    "Call_Error":"",
+    "Call_From":"TVy95wrbDgQ8NA3XT8qKqwC6YBsFTPHtFF",
+    "Call_Index":0,
+    "Call_Create":"false",
+    "Call_GasUsed":64285,
+    "Call_Gas":99498,
+    "Call_Success":"true",
+    "Call_Value":"0.000000",
+    "Call_To":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    "Contract_Address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    "Contract_Type":"TriggerSmartContract",
+    "Log_ExitIndex":0,
+    "Log_EnterIndex":0,
+    "Log_Index":0,
+    "Log_Pc":14635,
+    "Log_LogAfterCallIndex":0,
+    "Log_Signature_Name":"Transfer",
+    "Log_Signature_SignatureHash":"ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+    "Log_Signature_Signature":"Transfer(address,address,uint256)",
+    "Log_Signature_Type":"false",
+    "Log_Signature_Abi":"{\"inputs\":[{\"indexed\":true,\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"to\",\"type\":\"address\"},{\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"Event\"}",
+    "Log_SmartContract":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    "LogHeader_Address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    "LogHeader_Data":"0000000000000000000000000000000000000000000000000000000001cfe2eb",
+    "LogHeader_Index":0,
+    "LogHeader_Removed":"false",
+    "Transaction_Hash":"e122cba603b48ee234807f91859a04dbc4728f1b215d5c9c05385733bca674bc",
+    "Transaction_Fee":"0.000000",
+    "Transaction_Index":0,
+    "TransactionStatus_Success":"true"
+  }
