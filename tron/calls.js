80e4fd327ed8f458fcc7810b69fb8162fa297c9d
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-02-03 15:49:05 +0530

    Add tron calls data structure

---
+++ tron/calls.js
@@ -0,0 +1,57 @@
+// This is a sample of the tron_calls.parquet file
+// you can download more files from the following link: https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/tron/calls/78861100_78861149.parquet
+//    │   ├── 78861100_78861149.parquet
+//    │   ├── 78861150_78861199.parquet
+//    │   ├── 78861200_78861249.parquet
+  
+  {
+    "Arguments":"[[0, \"_to\", [], \"address\", [\"address\", \"a0b7ebbc810849d65b663a4f81c42078bd831ed5\", \"\", 0, 0, \"false\"]], [1, \"_value\", [], \"uint256\", [\"uint256\", \"\", \"163503000\", 0, 0, \"false\"]]]",
+    "Returns":"[]",
+    "StateChanges":"[[\"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t\", 0, \"f8ce2dbd412efae766347591623271aceeebceb24d0bb2e9d5b69177111b9631\", \"0000000000000000000000000000000000000000000000000000000000000000\"], [\"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t\", 0, \"ea46794a0262e7fc75299327c8f36741b2c535976fba0c1d5faa9826c3de9209\", \"000000000000000000000000000000000000000000000000000007f358c3ed71\"]]",
+    "Block_Number":78861100,
+    "Block_Date":1767225600000,
+    "Block_Time":1767246213,
+    "Call_CallPath":[
+
+    ],
+    "Call_CallerIndex":-1,
+    "Call_Create":"false",
+    "Call_Delegated":"false",
+    "Call_Depth":0,
+    "Call_EnterIndex":0,
+    "Call_Error":"",
+    "Call_ExitIndex":0,
+    "Call_From":"TQepFa5DCfJcgfmGuuZWmPByD8QkXzRS5W",
+    "Call_GasUsed":64285,
+    "Call_Gas":71542,
+    "Call_Input":"a9059cbb000000000000000000000000a0b7ebbc810849d65b663a4f81c42078bd831ed50000000000000000000000000000000000000000000000000000000009bedb98",
+    "Call_Index":0,
+    "Call_InternalCalls":0,
+    "Call_LogCount":1,
+    "Call_Opcode_Name":"",
+    "Call_Opcode_Code":0,
+    "Call_Output":"",
+    "Call_SelfDestruct":"false",
+    "Call_Reverted":"false",
+    "Call_Signature_Abi":"{\"outputs\":[{\"type\":\"bool\"}],\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"stateMutability\":\"Nonpayable\",\"type\":\"Function\"}",
+    "Call_Signature_Name":"transfer",
+    "Call_Signature_SignatureHash":"a9059cbb",
+    "Call_Signature_Signature":"transfer(address,uint256)",
+    "Call_Signature_Parsed":"true",
+    "Call_Signature_Type":"false",
+    "Call_ValueInUSD":0.0,
+    "Call_Value":"0.000000",
+    "Call_To":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    "Call_Success":"true",
+    "Contract_Address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    "Contract_Type":"TriggerSmartContract",
+    "Contract_TypeUrl":"type.googleapis.com\/protocol.TriggerSmartContract",
+    "Transaction_Hash":"6f3394ac6a160ca6eca0940c97cb59db511d43958e2be5f817ace7eaa82758c0",
+    "Transaction_Fee":"0.345000",
+    "TransactionStatus_Success":"true",
+    "TransactionStatus_EndError":"",
+    "TransactionStatus_FaultError":"",
+    "Witness_Address":"TDpt9adA6QidL1B1sy3D8NC717C6L5JxFo",
+    "Witness_Signature":"59730c777fac4ee3a4f25edd0b04edaf87c76728e3426b4d352bbde8fa5618fe070fdcba726e55c46e1ea2d2ef00730e1e2cae492e8a05b04429d9e76b72af0700"
+  }
+
