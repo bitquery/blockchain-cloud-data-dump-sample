32701a98442682ee1cbac4dc5953b9880c2f62a0
Akshat <akshatmeena368@gmail.com>
2026-07-21 14:08:01 +0530

    added pump fun dex pools and ohlcv sample

---
+++ solana/pumpfun_dex_pools.js
@@ -0,0 +1,40 @@
+// more files in s3 available, file links below
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/Pumpfun_Sample/dex_pools/415261250_415261499.parquet
+// ├── Pumpfun_Sample/
+// │   ├── dex_pools/
+// │   │   ├── 415261250_415261499.parquet
+// │   │   ├── 415261500_415261749.parquet
+// │   │   ├── 415261750_415261999.parquet
+// │   │   └── 415262000_415262249.parquet
+module.exports = [
+  {
+    "Block_Slot":415261455,
+    "Block_Time":"2026-04-24T03:10:16.000000Z",
+    "Pool_BaseCurrency_ChangeAmount":"873764705882353",
+    "Pool_BaseCurrency_ChangeAmount_InUSD":0.0,
+    "Pool_BaseCurrency_PostAmount":"873764705882353",
+    "Pool_BaseCurrency_Price":0.0,
+    "Pool_BaseCurrency_PostAmount_InUSD":0.0,
+    "Pool_BaseCurrency_Price_InUSD":0.0000000001,
+    "Pool_Dex_ProgramAddress":"6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P",
+    "Pool_Dex_ProtocolFamily":"Pumpfun",
+    "Pool_Dex_ProtocolName":"pump",
+    "Pool_Market_BaseCurrency_Decimals":6,
+    "Pool_Market_BaseCurrency_MintAddress":"2ra5idczuCQhDe1U5D52G8Rms6hzHuHeTqP51fdHpump",
+    "Pool_Market_MarketAddress":"7oCe5VtyC1GKDQyULho3igyFQJkB35Ji6XamegAg2XbZ",
+    "Pool_Market_QuoteCurrency_Decimals":9,
+    "Pool_Market_QuoteCurrency_MintAddress":"11111111111111111111111111111111",
+    "Pool_QuoteCurrency_ChangeAmount":"1468560",
+    "Pool_QuoteCurrency_ChangeAmount_InUSD":0.125929132,
+    "Pool_QuoteCurrency_PostAmount":"1468560",
+    "Pool_QuoteCurrency_PostAmount_InUSD":0.125929132,
+    "Pool_QuoteCurrency_Price_InUSD":0.0,
+    "Pool_QuoteCurrency_Price":594980597239.7131347656,
+    "PoolEvent_Index":0,
+    "Transaction_Signature":"61iqefxw42LJMA2ArcfMA2BiepZeVV4VGb8ZjVGkT8tKRUwosVVhoBC1zQmmBjhrQzn8HVTLHRyjkKXbcY3kTWfL",
+    "Transaction_Signer":"8ZN71XTdVo8yRovnGLmNgW3Tgniw6A4J3JGLvPD686FP",
+    "Transaction_Fee":"3110000",
+    "Transaction_Result_Success":"true",
+    "Transaction_Index":8
+  }
+];
---
+++ solana/pumpfun_ohlcv.js
@@ -0,0 +1,20 @@
+// more files in s3 available, file links below
+// https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/Pumpfun_Sample/ohlcv/2ra5idczuCQhDe1U5D52G8Rms6hzHuHeTqP51fdHpump.parquet
+// ├── Pumpfun_Sample/
+// │   ├── ohlcv/
+// │   │   └── <MintAddress>.parquet
+module.exports = [
+  {
+    "Time":"2026-04-24T03:10:00.000000Z",
+    "Open_Price_InUSD":"5.0425741756910417e-05",
+    "High_Price_InUSD":"0.00011462746479398489",
+    "Low_Price_InUSD":"3.9935168916976885e-05",
+    "Close_Price_InUSD":"6.250931925572058e-05",
+    "MintAddress":"2ra5idczuCQhDe1U5D52G8Rms6hzHuHeTqP51fdHpump",
+    "Side_MintAddress":"So11111111111111111111111111111111111111112",
+    "MarketAddress":"",
+    "ProtocolName":"jupiter",
+    "Volume_InUSD":"222.16779370489223",
+    "Trade_Count":15
+  }
+];
